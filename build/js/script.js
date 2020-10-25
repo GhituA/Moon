848d422f7e830bd3f7c54c3b676c6876b321bec7
GhituA <isthatpawz@gmail.com>
2020-10-25 14:16:19 +0200

    Исправляет фон шапки при отключенном JS

--- build/js/script.js
+++ build/js/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
 (function () {
-  var navList = document.querySelector('.main-nav');
+  var navList = document.querySelector('.page-header__container');
   var navBar = document.querySelector('.main-nav__bar');
   var navBtn = document.querySelector('.main-nav__toggle');
   var siteList = document.querySelector('.site-list');
 
   var setMainNavActive = function () {
     navBtn.addEventListener('click', function () {
-      navList.classList.toggle('main-nav--js');
+      navList.classList.toggle('page-header__container--js');
       navBtn.classList.toggle('main-nav__toggle--open');
       siteList.classList.toggle('site-list--js');
       navBar.classList.toggle('main-nav__bar--open');
     });
 
-    navList.classList.add('main-nav--js');
+    navList.classList.add('page-header__container--js');
     navBtn.classList.remove('main-nav__toggle--hidden');
   };
 
