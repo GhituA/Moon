e0cca3b5e6c11bdccc3f1c1467c1843dcddccf0c
GhituA <isthatpawz@gmail.com>
2020-10-25 05:13:01 +0200

    Добавляет стили шапки, форму и JS для меню

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,22 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var navList = document.querySelector('.main-nav');
+  var navBar = document.querySelector('.main-nav__bar');
+  var navBtn = document.querySelector('.main-nav__toggle');
+  var siteList = document.querySelector('.site-list');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  var setMainNavActive = function () {
+    navBtn.addEventListener('click', function () {
+      navList.classList.toggle('main-nav--js');
+      navBtn.classList.toggle('main-nav__toggle--open');
+      siteList.classList.toggle('site-list--js');
+      navBar.classList.toggle('main-nav__bar--open');
+    });
+
+    navList.classList.add('main-nav--js');
+    navBtn.classList.remove('main-nav__toggle--hidden');
+  };
+
+  setMainNavActive();
+})();
