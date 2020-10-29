f89e9d1d11a020e00bf46b93af31839b2f10c3ff
GhituA <isthatpawz@gmail.com>
2020-10-29 18:10:27 +0200

    Корректирует резину

--- source/js/script.js
+++ source/js/script.js
@@ -1,28 +1,37 @@
 'use strict';
 
 (function () {
-  var pageContainer = document.querySelector('.page-header');
-  var navBar = document.querySelector('.menu-bar');
-  var navBtn = document.querySelector('.menu-bar__toggle');
-  var siteList = document.querySelector('.main-nav__list');
-  var navLinks = document.querySelectorAll('.main-nav__link');
-  var mapBlock = document.querySelector('.map');
-  var mapContainer = document.querySelector('.map__container');
+  var page = document.querySelector('.page');
+  var pageContainer = page.querySelector('.page-header');
+  var navBar = page.querySelector('.menu-bar');
+  var navBtn = page.querySelector('.menu-bar__toggle');
+  var navLogo = page.querySelector('.menu-bar__logo');
+  var siteList = page.querySelector('.main-nav__list');
+  var navLinks = page.querySelectorAll('.main-nav__link');
+  var mapBlock = page.querySelector('.map');
+  var mapContainer = page.querySelector('.map__container');
+
 
   /*  JS menu  */
 
   var onMenuOpen = function () {
+    navLogo.classList.add('menu-bar__logo--hidden');
     pageContainer.classList.remove('page-header--js');
     navBtn.classList.add('menu-bar__toggle--open');
     siteList.classList.add('main-nav__list--js');
-    navBar.classList.remove('menu-bar--open');
+    navBar.classList.remove('menu-bar--closed');
+    navBar.classList.add('menu-bar--open');
+    page.classList.add('page--lock');
   };
 
   var onMenuClose = function () {
+    navLogo.classList.remove('menu-bar__logo--hidden');
+    page.classList.remove('page--lock');
     pageContainer.classList.add('page-header--js');
     navBtn.classList.remove('menu-bar__toggle--open');
     siteList.classList.remove('main-nav__list--js');
-    navBar.classList.add('menu-bar--open');
+    navBar.classList.add('menu-bar--closed');
+    navBar.classList.remove('menu-bar--open');
 
     for (var i = 0; i < navLinks.length; i++) {
       navLinks[i].removeEventListener('click', onMenuClose);
@@ -59,16 +68,19 @@
     mapBlock.classList.remove('map--no-js');
 
     var myMap = document.createElement('script');
-    myMap.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBk6uBmJpjAaLIfkYjehWvMqWQktKwuESw&language=ru&callback=initMap";
+    myMap.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBk6uBmJpjAaLIfkYjehWvMqWQktKwuESw&language=ru&callback=initMap';
     myMap.defer = true;
+    myMap.async = true;
+    myMap.type = 'text/javascript';
 
     window.initMap = function () {
       var map = new google.maps.Map(mapContainer, {
         center: {lat: 59.938635, lng: 30.323118},
         zoom: 15.5
       });
-    }
-      document.head.appendChild(myMap);
+      return map;
+    };
+    document.head.appendChild(myMap);
   }
 
   setMainNavActive();
