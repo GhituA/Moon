93765d221cae2fbcb4a586f7a3e68b435ca89c1f
GhituA <isthatpawz@gmail.com>
2020-10-29 04:14:51 +0200

    Меняет разметку шапки, корректирует стили по Pixel Perfect

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,11 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
+gulp.task('js', function () {
+  return gulp.src('source/js/script.js')
+      .pipe(gulp.dest('build/js'));
+});
+
 gulp.task("server", function () {
   server.init({
     server: "build/",
@@ -41,7 +46,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/js/*.js", gulp.series("refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -56,7 +61,6 @@ gulp.task("images", function() {
     ]))
 
     .pipe(gulp.dest("source/img"));
-
 });
 
 gulp.task("webp", function () {
--- source/js/script.js
+++ source/js/script.js
@@ -2,37 +2,35 @@
 
 (function () {
   var pageContainer = document.querySelector('.page-header');
-  var navBar = document.querySelector('.main-nav__bar');
-  var navBtn = document.querySelector('.main-nav__toggle');
-  var siteList = document.querySelector('.site-list');
-  // var mapBlock = document.querySelector('.map');
+  var navBar = document.querySelector('.menu-bar');
+  var navBtn = document.querySelector('.menu-bar__toggle');
+  var siteList = document.querySelector('.main-nav__list');
+  var mapBlock = document.querySelector('.map');
 
   /*  JS menu  */
 
   var setMainNavActive = function () {
     navBtn.addEventListener('click', function () {
       pageContainer.classList.toggle('page-header--js');
-      navBtn.classList.toggle('main-nav__toggle--open');
-      siteList.classList.toggle('site-list--js');
-      navBar.classList.toggle('main-nav__bar--open');
+      navBtn.classList.toggle('menu-bar__toggle--open');
+      siteList.classList.toggle('main-nav__list--js');
+      navBar.classList.toggle('menu-bar--open');
     });
 
     pageContainer.classList.add('page-header--js');
-    navBtn.classList.remove('main-nav__toggle--hidden');
+    navBtn.classList.remove('menu-bar__toggle--hidden');
   };
 
   /*  Map  */
-  //
-  // mapBlock.classList.remove('map--no-js');
-  //
-  // var map;
-  //
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById('map__container'), {
-  //     center: {lat: 59.938568, lng: 30.322747},
-  //     zoom: 16
-  //   });
-  // }
+
+  mapBlock.classList.remove('map--no-js');
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map__container'), {
+      center: {lat: 59.938635, lng: 30.323118},
+      zoom: 15.5
+    });
+  }
 
   setMainNavActive();
 })();
