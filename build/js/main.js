8adb0c7a80e0ff99b5d2ea167b72c0b5a25abf76
GhituA <isthatpawz@gmail.com>
2020-11-04 00:16:03 +0200

    Правит паттерн ввода телефона, добавляет скрипт для валидации полей ввода

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,49 @@
 
 (function () {
 
+  var cbForm = document.querySelector('form');
+  var cbPhone = cbForm.querySelector('#phone');
+  var cbName = cbForm.querySelector('#username');
+  var MIN_NAME_LENGTH = 4;
+  var MAX_NAME_LENGTH = 30;
+  var numbers = /^[\d\+ ]*$/;
+
+  var onPhoneInput = function (evt) {
+    var valueLength = evt.target.value.length;
+    if (!evt.target.value.match(numbers)) {
+      evt.target.setCustomValidity('Только числовые значения');
+    } else if (evt.target.validity.patternMismatch) {
+      evt.target.setCustomValidity('Пример ввода:  8 925 333 22 22');
+    } else if (valueLength < 1) {
+      evt.target.setCustomValidity('Это поле обязательно для заполнения');
+    } else {
+      evt.target.setCustomValidity('');
+    }
+  };
+
+  var onNameInput = function (evt) {
+    var valueLength = evt.target.value.length;
+    if (valueLength < 1) {
+      evt.target.setCustomValidity('Это поле обязательно для заполнения');
+    } else if (valueLength < MIN_NAME_LENGTH) {
+      evt.target.setCustomValidity('Минимальная длина ввода: 4 символов. Введите ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+    } else if (valueLength > MAX_NAME_LENGTH) {
+      evt.target.setCustomValidity('Максимальная длина ввода: 30 символов. Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+    } else {
+      evt.target.setCustomValidity('');
+    }
+  };
+
+
+  cbPhone.addEventListener('input', onPhoneInput);
+  cbName.addEventListener('input', onNameInput);
+
+})();
+
+'use strict';
+
+(function () {
+
   var mapBlock = document.querySelector('.map');
   var mapContainer = document.querySelector('.map__container');
 
